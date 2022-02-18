3196f94273736a6fde202a15fd37b30c137956c0
Teodor Muzychuk <teodor.muzychuk@ucu.edu.ua>
2022-02-18 19:24:46 +0200

    added more visualization

--- visualize.py
+++ visualize.py
@@ -26,7 +26,7 @@ def visualize_mstp(G):
     pos = nx.spring_layout(P)
     nx.draw(P, pos, with_labels=True)
     weights = dict([(data[0:2], data[2]["weight"]) for data in P.edges(data=True)])
-    print(f"Graph total weight: {sum(list(weights.values()))}")
+    print(f"Prim total weight: {sum(list(weights.values()))}")
     nx.draw_networkx_edge_labels(P, pos, edge_labels=weights)
 
     plt.figure(3)
@@ -34,7 +34,7 @@ def visualize_mstp(G):
     pos = nx.spring_layout(K)
     nx.draw(K, pos, with_labels=True)
     weights = dict([(data[0:2], data[2]["weight"]) for data in K.edges(data=True)])
-    print(f"Graph total weight: {sum(list(weights.values()))}")
+    print(f"Kruskal total weight: {sum(list(weights.values()))}")
     nx.draw_networkx_edge_labels(K, pos, edge_labels=weights)
 
     plt.figure(4)
@@ -42,7 +42,7 @@ def visualize_mstp(G):
     pos = nx.spring_layout(M)
     nx.draw(M, pos, with_labels=True)
     weights = dict([(data[0:2], data[2]["weight"]) for data in M.edges(data=True)])
-    print(f"Graph total weight: {sum(list(weights.values()))}")
+    print(f"Builtin algorithm total weight: {sum(list(weights.values()))}")
     nx.draw_networkx_edge_labels(M, pos, edge_labels=weights)
 
     plt.show()
@@ -50,8 +50,8 @@ def visualize_mstp(G):
 
 if __name__ == "__main__":
     G = gnp_random_connected_graph(8, 0.4)
-    t = perf_counter()
-    T = generate_mst_kruskal(G)
-    t = perf_counter() - t
-    print(t)
+    # t = perf_counter()
+    # T = generate_mst_kruskal(G)
+    # t = perf_counter() - t
+    # print(t)
     visualize_mstp(G)
